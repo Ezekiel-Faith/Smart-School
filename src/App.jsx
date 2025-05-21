2b7d24985de00a1ad4bb4121ef3bbd0e498b2b68
Ezekiel-faith <ezekielfaith20002gmail.com>
2025-05-21 18:29:18 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import About from './pages/About-Us';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <>
+      <Home />
+      <About />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/About-Us.jsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <div className='bg-amber-500'>About-Us</div>;
+}
