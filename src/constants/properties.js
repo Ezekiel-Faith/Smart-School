3f2dccf969ce7d00315626954e61153f32850521
Daniel <maddisonjames891@gmail.com>
2025-06-10 21:11:59 +0100

    Gallery Changes

---
+++ src/constants/properties.js
@@ -0,0 +1,27 @@
+import image1 from '../assets/1.png';
+import image2 from '../assets/2.png';
+import image3 from '../assets/3.png';
+import image4 from '../assets/4.png';
+import image5 from '../assets/5.png';
+import image6 from '../assets/6.png';
+import image7 from '../assets/7.png';
+import image8 from '../assets/8.png';
+import image9 from '../assets/9.png';
+import image10 from '../assets/10.png';
+import image11 from '../assets/11.png';
+import image12 from '../assets/12.png';
+
+export const Images = [
+    { id: "1", src: image1, alt: 'Image 1' },
+    { id: "2", src: image2, alt: 'Image 2' },
+    { id: "3", src: image3, alt: 'Image 3' },
+    { id: "4", src: image4, alt: 'Image 4' },
+    { id: "5", src: image5, alt: 'Image 5' },
+    { id: "6", src: image6, alt: 'Image 6' },
+    { id: "7", src: image7, alt: 'Image 7' },
+    { id: "8", src: image8, alt: 'Image 8' },
+    { id: "9", src: image9, alt: 'Image 9' },
+    { id: "10", src: image10, alt: 'Image 10' },
+    { id: "11", src: image11, alt: 'Image 11' },
+    { id: "12", src: image12, alt: 'Image 12' },
+  ];
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,38 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
 import '../Gallery.css'; 
-import image1 from '../assets/1.png';
-import image2 from '../assets/2.png';
-import image3 from '../assets/3.png';
-import image4 from '../assets/4.png';
-import image5 from '../assets/5.png';
-import image6 from '../assets/6.png';
-import image7 from '../assets/7.png';
-import image8 from '../assets/8.png';
-import image9 from '../assets/9.png';
-import image10 from '../assets/10.png';
-import image11 from '../assets/11.png';
-import image12 from '../assets/12.png';
+import '../index.css';
+import { Images } from '../constants/properties';
 
 export default function Gallery() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [isZoomed, setIsZoomed] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
   const galleryRef = useRef(null);
-
-  const images = [
-    { id: "1", src: image1, alt: 'Image 1' },
-    { id: "2", src: image2, alt: 'Image 2' },
-    { id: "3", src: image3, alt: 'Image 3' },
-    { id: "4", src: image4, alt: 'Image 4' },
-    { id: "5", src: image5, alt: 'Image 5' },
-    { id: "6", src: image6, alt: 'Image 6' },
-    { id: "7", src: image7, alt: 'Image 7' },
-    { id: "8", src: image8, alt: 'Image 8' },
-    { id: "9", src: image9, alt: 'Image 9' },
-    { id: "10", src: image10, alt: 'Image 10' },
-    { id: "11", src: image11, alt: 'Image 11' },
-    { id: "12", src: image12, alt: 'Image 12' },
-  ];
 
   useEffect(() => {
     // Slide-in animation trigger
@@ -74,14 +49,14 @@ export default function Gallery() {
   return (
     <>
       <div className={`px-4 sm:px-6 lg:px-8 py-8 transition-all duration-1000 ease-out ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}>
-        <section className='flex items-center flex-col gap-2 bg-[#FEFFFB]' ref={galleryRef}> 
+        <section className='flex items-center flex-col gap-2 bg-SoftLight' ref={galleryRef}> 
           <div>
-            <h1 className='text-center text-3xl font-project text-[32px] md:text-[49px] font-[700] leading-[1.2] my-10 bg-linear-to-bl from-[#ED7884] to-[#9055FF] bg-clip-text text-transparent'>
+            <h1 className='text-center text-3xl font-project text-[32px] md:text-[49px] font-[700] leading-[1.2] my-10 bg-linear-to-bl from-PurpleBlend to-PurpleBlend2 bg-clip-text text-transparent'>
               Explore Our Gallery
             </h1>
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full max-w-[1240px] gallery">
-            {images.map((image, index) => (
+            {Images.map((image, index) => (
               <div 
                 key={image.id} 
                 className="gallery-item w-full overflow-hidden rounded-lg shadow-md hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1 cursor-pointer opacity-0"
