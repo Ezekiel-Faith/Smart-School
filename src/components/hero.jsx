936ae92ea0e8951048692ebf2bb90bdf16788966
Daniel <maddisonjames891@gmail.com>
2025-06-28 18:01:59 +0100

    Correction

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,9 +4,9 @@ import { HeroProperties } from "@/constants/properties";
 export default function Hero(){
   return(
     <>
-        <section className='mb-10 md:w-auto'>
-      <div className='relative hero-bg p-30 md:w-auto md:m-10'>
-        <h1 className='hero-txt font-project text-[49px] normal-case font-[700] leading-[58.8px]'>{HeroProperties.title}</h1>
+        <section className='mb-25 md:w-auto'>
+      <div className='relative hero-bg p-45 md:w-auto md:m-10'>
+        <h1 className='-mt-20 hero-txt font-project text-[49px] normal-case font-[700] leading-[58.8px]'>{HeroProperties.title}</h1>
       </div>
       <div className='flex flex-col items-center justify-center p-4 m-7'>
         <h1 className='Admission-txt txt'>{HeroProperties.subtitle}</h1>
