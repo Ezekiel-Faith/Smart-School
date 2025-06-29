9a49edd43cad462f7c5c03df4768ea9f67e52ad3
ProsperMichael123 <135698730+ProsperMichael123@users.noreply.github.com>
2025-06-29 11:14:13 +0100

    correction code for the footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -8,24 +8,24 @@ const Footer = () => {
   return (
     <div>
       <footer>
-        <section className='bg-gray-900 w-100% h-250'>
-          <div className='grid grid-cols-4 pt-25 mx-20'>
+        <section className='bg-gray-900 w-100% h-220'>
+          <div className='grid grid-cols-4 pt-25 mx-30'>
             <div className='grid grid-cols-2 col-span-2 mt-5'>
               <img src={schoolLogo} alt="School Logo" className="h-16 rounded-full" />
-              <h1 className='text-purple-500 text-4xl -ml-65 mt-3 font-bold'>Smart School</h1>
+              <h1 className='text-purple-500 font-bold text-3xl -ml-60 mt-3 font-space-grotesk'>Smart School</h1>
             </div>
 
             <div className='grid grid-cols-4 gap-15 col-span-2 -mr-15 -ml-35'>
               <div>
-                <h1 className='text-white text-3xl font-semibold mb-7 mt-2'>Quick Links</h1>
-                <p className='text-gray-400 mb-3 text-lg'>Contact Us</p>
-                <p className='text-gray-400 mb-3 text-lg'>FAQs</p>
-                <p className='text-gray-400 mb-3 text-lg'>Gallery</p>
-                <p className='text-gray-400 mb-3 text-lg'>Admissions</p>
+                <h1 className='text-white text-2xl font-bold mb-7 mt-2 font-space-grotesk'>Quick Links</h1>
+                <p className='quick-link font-space-grotesk'>Contact Us</p>
+                <p className='quick-link font-space-grotesk'>FAQs</p>
+                <p className='quick-link font-space-grotesk'>Gallery</p>
+                <p className='quick-link font-space-grotesk'>Admissions</p>
               </div>
 
               <div>
-                <h1 className='text-white text-3xl mb-6 font-semibold'>Navigation</h1>
+                <h1 className='text-white text-2xl mb-6 font-bold font-space-grotesk'>Navigation</h1>
                 <p className='text-gray-400 mb-3 text-lg'>Mission</p>
                 <p className='text-gray-400 mb-3 text-lg'>Vission</p>
                 <p className='text-gray-400 mb-3 text-lg'>About Us</p>
@@ -33,14 +33,14 @@ const Footer = () => {
               </div>
 
               <div>
-                <h1 className='text-white text-3xl font-semibold mb-7 mt-2'>Get in touch</h1>
+                <h1 className='text-white text-2xl font-bold mb-7 mt-2 font-space-grotesk'>Get in touch</h1>
 
-                <div className='grid grid-cols-2 mb-7'>
-                  <div className='mt-8'>
-                    <FontAwesomeIcon icon={faLocationDot} style={{ color: 'white', fontSize: '24px' }} />
+                <div className='grid grid-cols-2 mb-5'>
+                  <div className='mt-3'>
+                    <FontAwesomeIcon icon={faLocationDot} style={{ color: 'white', fontSize: '18px' }} />
                   </div>
                   <div>
-                    <p className='text-gray-400 -ml-13'>
+                    <p className='text-gray-400 -ml-13 text-xs '>
                       123 Example Street,<br />
                       Victoria Island, Lagos,<br />
                       Nigeria
@@ -48,21 +48,21 @@ const Footer = () => {
                   </div>
                 </div>
 
-                <div className='grid grid-cols-2 mb-7'>
+                <div className='grid grid-cols-2 mb-5'>
                   <div>
-                    <FontAwesomeIcon icon={faEnvelope} style={{ color: 'white', fontSize: '24px' }} />
+                    <FontAwesomeIcon icon={faEnvelope} style={{ color: 'white', fontSize: '18px' }} />
                   </div>
                   <div>
-                    <p className='text-gray-400 -ml-13 text-lg'>info@example.com</p>
+                    <p className='text-gray-400 -ml-13 text-sm'>info@example.com</p>
                   </div>
                 </div>
 
                 <div className='grid grid-cols-2'>
                   <div>
-                    <FontAwesomeIcon icon={faPhone} style={{ color: 'white', fontSize: '24px' }} />
+                    <FontAwesomeIcon icon={faPhone} style={{ color: 'white', fontSize: '18px' }} />
                   </div>
                   <div>
-                    <p className='text-gray-400 -ml-13'>07012345677, 07012345677</p>
+                    <p className='text-gray-400 -ml-14 text-xs'>07012345677,07012345677</p>
                   </div>
                 </div>
               </div>
@@ -70,7 +70,7 @@ const Footer = () => {
           </div>
 
           <div className='grid grid-cols-2 gap-45 pt-20'>
-            <div className='mt-15'>
+            <div className='mt-15 mx-5'>
               <p className='text-gray-400 text-xl pl-27 pb-10'>
                 Questions or updates? Follow us on social media,<br />
                 Connect with us!
@@ -82,7 +82,7 @@ const Footer = () => {
                   rel="noopener noreferrer"
                   className="hover:scale-110 transition-transform duration-300 transform"
                 >
-                  <FontAwesomeIcon icon={faFacebook} style={{ color: 'white', fontSize: '60px' }} />
+                  <FontAwesomeIcon icon={faFacebook} style={{ color: 'white', fontSize: '53px' }} />
                 </a>
 
                 <a
@@ -91,7 +91,7 @@ const Footer = () => {
                   rel="noopener noreferrer"
                   className="hover:scale-110 transition-transform duration-300 transform"
                 >
-                  <FontAwesomeIcon icon={faTwitter} style={{ color: 'white', fontSize: '60px' }} />
+                  <FontAwesomeIcon icon={faTwitter} style={{ color: 'white', fontSize: '53px' }} />
                 </a>
 
                 <a
@@ -100,7 +100,7 @@ const Footer = () => {
                   rel="noopener noreferrer"
                   className="hover:scale-110 transition-transform duration-300 transform"
                 >
-                  <FontAwesomeIcon icon={faInstagram} style={{ color: 'white', fontSize: '60px' }} />
+                  <FontAwesomeIcon icon={faInstagram} style={{ color: 'white', fontSize: '53px' }} />
                 </a>
 
                 <a
@@ -109,30 +109,30 @@ const Footer = () => {
                   rel="noopener noreferrer"
                   className="hover:scale-110 transition-transform duration-300 transform"
                 >
-                  <FontAwesomeIcon icon={faWhatsapp} style={{ color: 'white', fontSize: '60px' }} />
+                  <FontAwesomeIcon icon={faWhatsapp} style={{ color: 'white', fontSize: '53px' }} />
                 </a>
               </div>
             </div>
 
-            <div>
-              <h1 className='text-white text-5xl pb-9'>Our Newsletters</h1>
-              <p className='text-gray-400 text-xl pb-10'>
+            <div className='mr-30'>
+              <h1 className='text-white text-5xl pb-9 font-space-grotesk font-bold'>Our Newsletters</h1>
+              <p className='text-gray-400 text-2xs pb-10 font-space-grotesk'>
                 Subcribe to our newsletter and get exclusive insights, tips, and the <br />
                 latest trends delivered straight to your inbox. Don't miss out.
               </p>
               <button
                 placeholder="Enter Your Email Address"
-                className='bg-purple-500 placeholder-white w-135 h-17 rounded-4xl'
+                className='bg-purple-500 placeholder-white w-125 h-15 rounded-4xl'
               >
-                <button className='bg-purple-950 text-white w-45 h-16 rounded-4xl text-xl ml-90'>
+                <button className='bg-purple-950 text-white w-40 h-14 rounded-4xl text-xl ml-85 font-space-grotesk'>
                   Subscribe
                 </button>
               </button>
             </div>
           </div>
 
-          <hr className='border border-gray-500 w-335 mt-25 ml-27' />
-          <p className='text-gray-400 text-lg pl-120 pt-8'>
+          <hr className='border border-gray-500 w-300 mt-25 ml-33 ' />
+          <p className='text-gray-400 text-lg pl-110 pt-8 font-space-grotesk'>
             @ 2025 Smart School | All rights reserved | Tems and Conditions | Privacy Policy
           </p>
         </section>
