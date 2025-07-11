9a345174c37b8d1ec5b72a77b4ceb493abef3f59
Ezekiel-faith <ezekielfaith2000@gmail.com>
2025-07-11 13:33:50 +0100

    nonsense

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import FooterTop from './Component/FooterTop';
 import FooterDwn from './Component/FooterDwn';
 
-
-
-
 const Footer = () => {
   return (
     <footer className='bg-gray-900 '>
