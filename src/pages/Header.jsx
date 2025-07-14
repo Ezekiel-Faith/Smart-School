198ff591e3478bce00a9d0a001ed32cf66002d74
Daniel <maddisonjames891@gmail.com>
2025-07-14 13:21:20 +0100

    hovers

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -29,17 +29,17 @@ function Header() {
         <img src={logo} alt="Logo" className="w-[98px] h-[92.505px]"/>
       </Link>
       <nav className='flex items-center gap-2'>
-        <ul className='flex items-center gap-12'>
+        <ul className='flex items-center gap-10'>
           {NavLinks.map((link) => (
-            <li key={link.id} className='mr-1'>
+            <li key={link.id} className=''>
               <Link
                 to={`${link.path}`}
                 onClick={() => setActiveLink(link.id)}
                 className={`
-                  text-[17px] font-[600] font-project transition-all text-PurpleBlend duration-300
+                  text-[16px] font-[600] font-project transition-all text-PurpleBlend duration-300
                   ${activeLink === link.id 
-                    ? 'text-[18px] font-[600] underline underline-offset-8 text-blue-900 decoration-blue-900 decoration-2'
-                    : 'text-project hover:text-gray-300 '
+                    ? 'text-[17px] font-[600] underline underline-offset-5 text-blue-900 decoration-blue-900 decoration-2'
+                    : 'text-project links'
                   }
                 `}
               >
@@ -51,7 +51,7 @@ function Header() {
       </nav>
       
       <button 
-        className='btn mr-3 font-project p-2 rounded-[8px] w-[189px] h-[57px] px-[22px] py-[16px] cursor-pointer relative text-white register-btn transition-all duration-300 hover:bg-gradient-to-[233deg]'
+        className='btn mr-3 font-project p-2 rounded-[8px] w-[189px] h-[57px] px-[22px] py-[16px] cursor-pointer relative text-white register-btn transition duration-1000 ease-in-out hover:bg-gradient-to-[233deg]'
       >
         Register
       </button>
