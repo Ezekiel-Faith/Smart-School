0423e5f03ee6823116459acb2f25d860c0a235dc
Ezekiel-faith <ezekielfaith2000@gmail.com>
2025-07-20 13:33:03 +0100

    parameter fixed

---
+++ src/pages/Contact-Us.jsx
@@ -0,0 +1,25 @@
+import Banner from '@/components/Banner';
+import Card from '@/components/Card';
+import ContactSection from '@/components/ContactSection';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function Contact() {
+  const { pageTitle } = useParams();
+
+  const formattedTitle = pageTitle
+    ? pageTitle
+        .replace(/-/g, ' ')
+        .split(' ')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ')
+    : '';
+
+  return (
+    <div className='about-container'>
+      <Banner title={formattedTitle} />
+      <Card />
+      <ContactSection />
+    </div>
+  );
+}
