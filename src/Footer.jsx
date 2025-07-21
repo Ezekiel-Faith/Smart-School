4a4c9847526157a02b4abccbca221ae38c2d801c
ProsperMichael123 <135698730+ProsperMichael123@users.noreply.github.com>
2025-07-21 11:37:06 +0100

    done

---
+++ src/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import FooterTop from './Component/FooterTop';
+import FooterDwn from './Component/FooterDwn';
+
+
+
+
+const Footer = () => {
+  return (
+  //  done with the code 
+   <footer className="bg-gray-900 ">
+  <div className="max-w-9xl  px-4 md:px-0 scale-95 ">
+    <FooterTop />
+    <FooterDwn />
+  </div>
+</footer>
+
+  );
+};
+
+export default Footer;
