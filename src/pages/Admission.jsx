297c4a484f317118612b8d71228f26fab974967c
Ezekiel-faith <ezekielfaith2000@gmail.com>
2025-07-23 17:04:41 +0100

    admission merged

--- src/pages/Admission.jsx
+++ src/pages/Admission.jsx
@@ -4,15 +4,14 @@ import Hero from '@/components/hero';
 import Study from '@/components/study';
 import DownloadNews from '@/components/News';
 
-export default function Admission(){
-
-  return(
+export default function Admission() {
+  return (
     <>
-    <div className='lg:mx-30 mx-5'>    
-    <Hero />
-    <Study />
-    <DownloadNews />
-    </div>
+      <div className='container'>
+        <Hero />
+        <Study />
+        <DownloadNews />
+      </div>
     </>
   );
 }
