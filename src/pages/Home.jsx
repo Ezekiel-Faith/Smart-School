a926f6ab91fd48eb73e70c3ad2f9c272827888cc
Ezekiel-faith <ezekielfaith2000@gmail.com>
2025-07-25 20:47:32 +0100

    nth changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,30 @@
-
-import ImageSlider from '@/Component/imageSlider'
-import MarqueeSection from '@/Component/MarqueeSection'
-import Mission from '@/Component/Mission'
-import Principal from '@/Component/Principal'
-import SliderButton from '@/Component/sliderButton'
-import Testimonials from '@/Component/Testimonials'
-import Vission from '@/Component/Vission'
-import WhySmartSchool from '@/Component/whySmartSchool'
-import React from 'react'
+import ImageSlider from '@/Component/imageSlider';
+import MarqueeSection from '@/Component/MarqueeSection';
+import Mission from '@/Component/Mission';
+import Principal from '@/Component/Principal';
+import SliderButton from '@/Component/sliderButton';
+import Testimonials from '@/Component/Testimonials';
+import Vission from '@/Component/Vission';
+import WhySmartSchool from '@/Component/whySmartSchool';
+import React from 'react';
 
 const Home = () => {
   return (
     <>
-     <ImageSlider/>
-     <SliderButton/>
-     <MarqueeSection/>
+      <ImageSlider />
+      <SliderButton />
+      <MarqueeSection />
 
-     <div className='about-container'>
-      
-     <WhySmartSchool/>
-     <Mission/>
-      <Vission/>
-      <Principal/>
+      <div className='about-container'>
+        <WhySmartSchool />
+        <Mission />
+        <Vission />
+        <Principal />
       </div>
 
-     <Testimonials/>
+      <Testimonials />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
