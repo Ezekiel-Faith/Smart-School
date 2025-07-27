bd1d05f4ba82c91cdaf2fb054e8bde1da953f815
ProsperMichael123 <135698730+ProsperMichael123@users.noreply.github.com>
2025-07-27 02:21:23 +0100

    done with the footer page

--- src/Component/FooterDwn.jsx
+++ src/Component/FooterDwn.jsx
@@ -27,26 +27,26 @@ export default function FooterDwn() {
       animate="visible"
     >
       {/* Main Container */}
-      <div className="flex p-3 lg:py-4 w-full flex-col lg:flex-row justify-between items-center m-auto">
-        
+      <div className="flex p-3 lg:py-4 w-full flex-col lg:flex-row justify-between md:items-start xl:items-center m-auto">
+
         {/* Left Column: Social Media */}
         <motion.div
           className="mb-5 lg:mb-0 lg:w-1/2 flex flex-col items-center order-last lg:order-none"
           variants={easeOutCenter}
         >
-          <p className="text-gray-400 text-xs lg:text-xl mb-1 lg:mb-6 text-start">
+          <p className="text-gray-400 text-base md:text-2xl lg:text-xl  lg:px-0 mb-1 lg:mb-6 text-start">
             Questions or updates? Follow us on social media,
             <br />
             Connect with us!
           </p>
-          <div className="flex items-start justify-center lg:justify-start lg:-ml-25">
+          <div className="flex items-start justify-center lg:justify-start lg:-ml-25 pr-10 md:pr-45 lg:pr-0 ">
             <a
               href="https://www.facebook.com"
               target="_blank"
               rel="noopener noreferrer"
               className="hover:scale-110 transition-transform duration-300 transform p-7"
             >
-              <FontAwesomeIcon icon={faFacebook} className="text-white text-4xl lg:text-5xl" />
+              <FontAwesomeIcon icon={faFacebook} className="text-white text-4xl md:text-5xl lg:text-5xl" />
             </a>
             <a
               href="https://www.twitter.com"
@@ -54,7 +54,7 @@ export default function FooterDwn() {
               rel="noopener noreferrer"
               className="hover:scale-110 transition-transform duration-300 transform p-7"
             >
-              <FontAwesomeIcon icon={faTwitter} className="text-white text-4xl lg:text-5xl" />
+              <FontAwesomeIcon icon={faTwitter} className="text-white text-4xl md:text-5xl lg:text-5xl" />
             </a>
             <a
               href="https://www.instagram.com"
@@ -62,7 +62,7 @@ export default function FooterDwn() {
               rel="noopener noreferrer"
               className="hover:scale-110 transition-transform duration-300 transform p-7"
             >
-              <FontAwesomeIcon icon={faInstagram} className="text-white text-4xl lg:text-5xl" />
+              <FontAwesomeIcon icon={faInstagram} className="text-white text-4xl md:text-5xl lg:text-5xl" />
             </a>
             <a
               href="https://wa.me/2347012345678"
@@ -70,7 +70,7 @@ export default function FooterDwn() {
               rel="noopener noreferrer"
               className="hover:scale-110 transition-transform duration-300 transform p-7"
             >
-              <FontAwesomeIcon icon={faWhatsapp} className="text-white text-4xl lg:text-5xl" />
+              <FontAwesomeIcon icon={faWhatsapp} className="text-white text-4xl md:text-5xl lg:text-5xl" />
             </a>
           </div>
         </motion.div>
@@ -80,21 +80,21 @@ export default function FooterDwn() {
           className="justify-end lg:w-1/2 flex flex-col items-center lg:items-start mb-6 lg:mb-0 order-first lg:order-none"
           variants={easeOutCenter}
         >
-          <h1 className="text-white text-4xl lg:text-2xl  pb-4  font-space-grotesk font-bold text-start lg:text-left">
+          <h1 className="text-white text-3xl md:text-4xl lg:text-3xl pb-2 font-space-grotesk font-bold text-start pr-20 md:mr-70 xl:mr-0 lg:pr-0">
             Our Newsletters
           </h1>
-          <p className="text-gray-400 text-sm lg:text-base p-6 font-space-grotesk text-start lg:text-center ">
+          <p className="text-gray-400 text-base md:text-2xl lg:text-base py-6 font-space-grotesk text-start">
             Subscribe to our newsletter and get exclusive insights, tips, and the
             <br />
             latest trends delivered straight to your inbox. Don't miss out.
           </p>
-          <div className="relative flex w-full max-w-lg px-4 sm:px-0">
+          <div className="relative flex w-full max-w-lg md:max-w-2xl xl:max-w-lg">
             <input
               type="email"
               placeholder="Enter Your Email Address"
-              className="flex-1 bg-purple-700 placeholder-white pl-4 pr-32 py-3 rounded-4xl focus:outline-none focus:ring-2 focus:ring-purple-500 text-white"
+              className="flex-1 bg-purple-700 placeholder-white pl-4  pr-32  py-3 rounded-4xl focus:outline-none focus:ring-2 focus:ring-purple-500 text-white"
             />
-            <button className="absolute inset-y-0 right-4 lg:right-0 bg-purple-900 text-white px-4 py-2 rounded-4xl text-sm font-space-grotesk hover:bg-purple-800 transition-colors duration-200 h-10 mt-1">
+            <button className="absolute inset-y-0 right-0 bg-purple-900 text-white px-4 py-2 rounded-4xl text-sm font-space-grotesk hover:bg-purple-800 transition-colors duration-200 h-10 mt-1">
               Subscribe
             </button>
           </div>
@@ -103,11 +103,11 @@ export default function FooterDwn() {
 
       {/* Footer Bottom Line */}
       <motion.hr
-        className="border-t border-gray-700 lg:mt-5 max-w-6xl mx-auto"
+        className="border-t border-gray-700 lg:mt-5 max-w-6xl mx-auto "
         variants={easeOutCenter}
       />
       <motion.p
-        className="text-gray-400 text-sm text-center pt-5"
+        className="text-gray-400 text-sm md:text-lg xl:text-sm text-center pt-5"
         variants={easeOutCenter}
       >
         &copy; 2025 Smart School | All rights reserved | Terms and Conditions | Privacy Policy
