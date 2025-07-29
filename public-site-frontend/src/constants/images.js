d44f1c074d6e08c77359b4ba0fd75b46910941c6
Ezekiel-Faith <59092893+Ezekiel-Faith@users.noreply.github.com>
2025-07-29 13:00:38 +0100

    Delete public-site-frontend directory

--- public-site-frontend/src/constants/images.js
+++
@@ -1,6 +0,0 @@
-import img from '../assets/images/image.png';
-import img1 from '../assets/images/image.png';
-import img2 from '../assets/images/image.png';
-import img3 from '../assets/images/image.png';
-
-export const images = [img, img1, img2, img3];
--- public-site-frontend/src/constants/navlinks.js
+++
@@ -1,8 +0,0 @@
-export const NavLinks = [
-  { id: 'home', name: 'Home', path: '/' },
-  { id: 'about', name: 'About Us', path: '/about-us' },
-  { id: 'gallery', name: 'Gallery', path: '/gallery' }, // future route
-  { id: 'admissions', name: 'Admissions', path: '/admission' }, // future route
-  { id: 'academics', name: 'Academics', path: '/academics' },
-  { id: 'contact', name: 'Contact Us', path: '/contact' },
-];
