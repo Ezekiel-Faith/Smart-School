10d6adbe47e577021746b8c537c51ed9bac38a95
Ezekiel-Faith <ezekielfaith2000@gmail.com>
2025-10-01 11:08:45 +0100

    message

--- student-dashboard-frontend/src/App.jsx
+++ student-dashboard-frontend/src/App.jsx
@@ -9,3 +9,9 @@ function App() {
 }
 
 export default App;
+
+// what to fix later on
+// 1. Message when quiz is completed
+// 2. Grading system
+// 3. Generate results summary per subject selected
+// 4. Make question and options random, not static
